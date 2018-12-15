d8840fcb204343b83b53598fc41e64c2b2dc4af1
Michał <jankowski.michal@me.com>
2018-12-15 02:00:59 +0100

    👨🏻‍🚀 That was she said

--- PortKnocking/src/Main.java
+++ PortKnocking/src/Main.java
@@ -1,16 +1,21 @@
 import java.util.Arrays;
+import java.util.List;
 import java.util.stream.Collectors;
 
 public class Main {
 	public static void main(String[] args){
 
-		/*
-			TODO: Check if ports are > 1024
-		 */
 		if(args.length > 1) {
 			switch (args[0]) {
 				case "-s":
-					new Server(Arrays.stream(args).skip(1).map(Integer::valueOf).distinct().collect(Collectors.toList())).start();
+					List<Integer> ports = Arrays.stream(args).skip(1).map(Integer::valueOf).distinct().collect(Collectors.toList());
+
+					if(ports.stream().anyMatch(e -> e <= 1024)){
+						System.out.println("Knocker: You cannot use ports smaller than 1024");
+						System.exit(0);
+					}
+					new Server(ports).start();
+
 					break;
 				case "-c":
 					new Client(args[1], Arrays.stream(args).skip(2).map(Integer::valueOf).collect(Collectors.toList())).start();
